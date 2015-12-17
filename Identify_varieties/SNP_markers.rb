172566d2d2bc89b0e3e575f0eb56626d53bbc516
Pilar Corredor <Pilar.Moreno@tgac.ac.uk>
2015-12-17 11:15:58 +0000

    final version of script - working now

--- Identify_varieties/SNP_markers.rb
+++ Identify_varieties/SNP_markers.rb
@@ -2,20 +2,16 @@ require 'csv'
 require 'pp'
 
 f = File.open("SNPAndContig.csv", "r")
-reference = File.open("LIB12451_Reference_greater_2x.tab", "r")
-snps = File.open("SNPs/20X/LIB12451_SNP_freq_20x.txt", 'r')
+reference = File.open("LIB4468/LIB4468_Reference_greater_2x.tab", "r")
+snps = File.open("LIB4468/SNPs/10X/LIB4468_SNP_freq_10x.txt", 'r')
 
-
-# f = File.open("test_markers", "r")
-# reference = File.open("test_tab", "r")
-# snps = File.open("test_20x", 'r')
 
 pos_bases = {}
-contig_pos_snp = {}
 marker_pos = {}
 marker_contig_pos = {}
-con_pos_snp_poly = {}
-pos_alt = {}
+
+marker_contig_pos = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
+contig_pos_snp = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
 
 ####SNPs with markers file 
 f.each_line { |line|
@@ -24,57 +20,46 @@ f.each_line { |line|
   ref_alt = fields[1].strip 
   contig = fields[2].strip 
   pos = fields[3].strip
-  pos_bases.store(pos, ref_alt)
-  marker_pos.store(pos, marker)
-  contig_pos_snp.store(contig, pos_bases)
-  marker_contig_pos.store(contig, marker_pos)
+  contig_pos_snp[contig][pos] = ref_alt 
+  marker_contig_pos[contig][pos] = marker
 
  }
 
 
+con_pos_snp_poly = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
+pp "Finished with the SNP markers!"
+
+
 ####20X SNPs from SNP calling
 snps.each_line { |snp| 
-	polymorphims = snp.split("\t")
+  polymorphims = snp.split("\t")
   freq = polymorphims[-1].strip
-  if freq.to_f >= 0.85 #prefiltering step 
-    alt_base = polymorphims[3].strip
-    contig_poly = polymorphims[0].strip
-    pos_poly = polymorphims[2].strip
-    pos_alt.store(pos_poly, alt_base)
-    con_pos_snp_poly.store(contig_poly, pos_alt)
-  # elsif freq.length > 5 
-  #   pp freq
-  #   i = i +1
-    # if freq 
-    #  two_bases = polymorphims[5].strip
+  alt_base = polymorphims[5].strip
+  contig_poly = polymorphims[0].strip
+  pos_poly = polymorphims[2].strip
+  con_pos_snp_poly[contig_poly][pos_poly] = alt_base
+}
 
-  
+
+pp "Finished opening the 10x SNP file"
+
+
+final_contig_pos = Hash.new{ |h,k| h[k] = Hash.new(&h.default_proc) }
+
+##Reference tab file 
+reference.each_line { |ref| 
+  columns = ref.split("\t")
+  ref_sample = columns[2].strip
+  contig_sample = columns[0].strip
+  pos_sample = columns[1].strip
+  if con_pos_snp_poly.has_key?(contig_sample)
+    if con_pos_snp_poly[contig_sample].has_key?(pos_sample)
+      base_alt = con_pos_snp_poly[contig_sample][pos_sample] 
+      final_contig_pos[contig_sample][pos_sample] = base_alt
+    end
   end 
 }
-
-
-
-final_contig_pos = {}
-final_pos_bases = {}
-bases = []
-
-####Reference tab file 
-reference.each_line { |ref| 
-	columns = ref.split("\t")
-	ref_sample = columns[2].strip
-	contig_sample = columns[0].strip
-	pos_sample = columns[1].strip
-	if con_pos_snp_poly.has_key?(contig_sample)
-		if con_pos_snp_poly[contig_sample].has_key?(pos_sample)
-      base_alt = con_pos_snp_poly[contig_sample][pos_sample] 
-      # bases << ref_sample + base_alt 
-      final_pos_bases.store(pos_sample, ref_sample + base_alt)
-      final_contig_pos.store(contig_sample, final_pos_bases)
-      # pp final_contig_pos
- 		end
-	end 
-}
-
+pp "Finished combining 10x and reference files"
 
 
 
@@ -86,19 +71,30 @@ final_contig_pos.each do |contig, pos_base|
         base_ref = contig_pos_snp[contig][pos][0]
         base_alt = contig_pos_snp[contig][pos][-1]
         name_marker = marker_contig_pos[contig][pos]
-        if base_alt == ref_alt_snp[-1] && base_ref == ref_alt_snp[0]
-            CSV.open("snps_good.csv", 'ab') do |csv|
+        if ref_alt_snp[-2] == ref_alt_snp[-1]   
+          if base_alt == ref_alt_snp[-1]
+            CSV.open("snps_good_last.csv", 'ab') do |csv|
               csv << [name_marker, contig, pos, base_ref, base_alt, 2]
             end
-        elsif base_alt == ref_alt_snp[0]
-          CSV.open("snps_good.csv", 'ab') do |csv|
-            csv << [name_marker, contig, pos, ref_alt_snp[0], base_alt, 0]
+          elsif base_alt == ref_alt_snp[0]
+
+            CSV.open("snps_good_last.csv", 'ab') do |csv|
+              csv << [name_marker, contig, pos, ref_alt_snp[0], base_alt, 0]
+            end
           end
+        elsif ref_alt_snp[-2] != ref_alt_snp[-1]   
+          if ref_alt_snp[-1] == base_alt || ref_alt_snp[-2] == base_alt
+            CSV.open("snps_good_last.csv", 'ab') do |csv|
+              csv << [name_marker, contig, pos, base_ref, base_alt, 1]
+            end
+          end 
         end 
-      end
+      end 
     end 
   end 
 end 
 
+
+pp "Done"
 
 
