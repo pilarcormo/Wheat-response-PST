df732e5b377c7a929121463b491dae9a2af17dcb
Pilar Corredor <Pilar.Moreno@tgac.ac.uk>
2016-03-18 17:02:16 +0000

    cummeRbund update

--- Differential_expression/cummeRbund.R
+++ Differential_expression/cummeRbund.R
@@ -2,6 +2,7 @@
 
 source("https://bioconductor.org/biocLite.R")
 biocLite("cummeRbund")
+install.packages("sqldf")
 
 
 dbFile<-paste('~/Differential_expression/cuffdiff',"/cuffData.db", sep="")
@@ -20,11 +21,6 @@ sig_gene_data <- subset(gene_diff_data, (significant == 'yes'))
 diffGenes<-getGenes(cuff, sig_gene_data$gene_id)
 names<-featureNames(diffGenes)
 
-g <- genes(cuff)
-
-csDensity(genes(cuff)) ###distribution ofRNA-seq read counts (fpkm)
-csVolcanoMatrix(g) 
-csScatter(g)
 
 
 myGeneIds<- c("Oakley","Santiago", "Cordiale")
@@ -40,19 +36,53 @@ genes.scv<-fpkmSCVPlot(genes(cuff))
 genes.scv
 isoforms.scv<-fpkmSCVPlot(isoforms(cuff))
 isoforms.scv
+
+###distribution ofRNA-seq read counts (fpkm)
 dens<-csDensity(genes(cuff))
 dens
 densRep<-csDensity(genes(cuff),replicates=T)
 brep<-csBoxplot(genes(cuff),replicates=T)
 brep
+
+##scatter okit 
 scatter<-csScatterMatrix(genes(cuff))
 scatter
- 
 scatter<-csScatterMatrix(genes(cuff),replicates=T)
 scatter
+
+###volcano plot
 v<-csVolcanoMatrix(genes(cuff))
-v
 v<-csVolcanoMatrix(genes(cuff), replicates=T)
-v
 tab<-getSigTable(cuff)
 
+##overview of significant features 
+mySigMat<-sigMatrix(cuff,level='genes',alpha=0.05) 
+
+###Distance matrix
+myDistHeat<-csDistHeat(genes(cuff))
+myDistHeat
+
+#######Dimensionality reduction
+###PCA
+genes.PCA<-PCAplot(genes(cuff),"PC1","PC2")
+genes.PCA 
+
+genes.MDS.rep<-MDSplot(genes(cuff),replicates=T) 
+genes.MDS.rep
+
+####Gene clusters 
+ic <- csCluster(diffGenes, k=6) 
+head(ic)
+
+featureNames(ic)
+head(ic$cluster)  
+icp<-csClusterPlot(ic) 
+icp
+write.table(ic$clustering, "clusters.txt")
+
+
+myGenes.spec<-csSpecificity(diffGenes)  
+
+##Heatmap
+h<-csHeatmap(myGenes,cluster='both') 
+h
