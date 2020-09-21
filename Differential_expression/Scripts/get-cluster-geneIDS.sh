53be9ff94f246b51d0787fb053cd38d8a17a83f9
Pilar Corredor <Pilar.Moreno@tgac.ac.uk>
2020-09-21 10:49:50 +0200

    update

---
+++ Differential_expression/Scripts/get-cluster-geneIDS.sh
@@ -0,0 +1,31 @@
+
+
+awk -F '\t' '{print $2}' 1314_OA2/Clusters_Objects.tsv > 1314_OA2/1314-OA-C1.txt
+awk -F '\t' '{print $3}' 1314_OA2/Clusters_Objects.tsv > 1314_OA2/1314-OA-C2.txt
+awk -F '\t' '{print $4}' 1314_OA2/Clusters_Objects.tsv > 1314_OA2/1314-OA-C3.txt
+awk -F '\t' '{print $5}' 1314_OA2/Clusters_Objects.tsv > 1314_OA2/1314-OA-C4.txt
+awk -F '\t' '{print $6}' 1314_OA2/Clusters_Objects.tsv > 1314_OA2/1314-OA-C5.txt
+awk -F '\t' '{print $7}' 1314_OA2/Clusters_Objects.tsv > 1314_OA2/1314-OA-C6.txt
+awk -F '\t' '{print $8}' 1314_OA2/Clusters_Objects.tsv > 1314_OA2/1314-OA-C7.txt
+awk -F '\t' '{print $9}' 1314_OA2/Clusters_Objects.tsv > 1314_OA2/1314-OA-C8.txt
+awk -F '\t' '{print $10}' 1314_OA2/Clusters_Objects.tsv > 1314_OA2/1314-O2-C9.txt
+awk -F '\t' '{print $11}' 1314_OA2/Clusters_Objects.tsv > 1314_OA2/1314-O2-C10.txt
+awk -F '\t' '{print $12}' 1314_OA2/Clusters_Objects.tsv > 1314_OA2/1314-O2-C11.txt
+awk -F '\t' '{print $13}' 1314_OA2/Clusters_Objects.tsv > 1314_OA2/1314-O2-C12.txt
+awk -F '\t' '{print $14}' 1314_OA2/Clusters_Objects.tsv > 1314_OA2/1314-O2-C13.txt
+
+
+
+awk -F '\t' '{print $1}' Clusters_Objects.tsv > 1314-SO-C0.txt
+awk -F '\t' '{print $2}' Clusters_Objects.tsv > 1314-SO-C1.txt
+awk -F '\t' '{print $3}' Clusters_Objects.tsv > 1314-SO-C2.txt
+awk -F '\t' '{print $4}' Clusters_Objects.tsv > 1314-SO-C3.txt
+awk -F '\t' '{print $5}' Clusters_Objects.tsv > 1314-SO-C4.txt
+awk -F '\t' '{print $6}' Clusters_Objects.tsv > 1314-SO-C5.txt
+awk -F '\t' '{print $7}' Clusters_Objects.tsv > 1314-SO-C6.txt
+awk -F '\t' '{print $8}' Clusters_Objects.tsv > 1314-SO-C7.txt
+awk -F '\t' '{print $9}' Clusters_Objects.tsv > 1314-SO-C8.txt
+awk -F '\t' '{print $10}' Clusters_Objects.tsv > 1314-SO-C9.txt
+awk -F '\t' '{print $11}' Clusters_Objects.tsv > 1314-SO-C10.txt
+awk -F '\t' '{print $12}' Clusters_Objects.tsv > 1314-SO-C11.txt
+awk -F '\t' '{print $13}' Clusters_Objects.tsv > 1314-SO-C12.txt
